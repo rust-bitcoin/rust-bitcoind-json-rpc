383c1cf947c088037a2fdf7b701180e26ed03467
Elias Rohrer <dev@tnull.de>
2024-08-30 11:57:26 +0200

    WIP: Add support for Bitcoin Core v28
    Add support and testing for Bitcoin Core versions `28.0`

---
+++ integration_test/tests/v28_api.rs
@@ -0,0 +1,57 @@
+//! Test the JSON-RPC API against `bitcoind v28.0`.
+
+#![cfg(feature = "v28")]
+
+use integration_test::*;
+
+// == Blockchain ==
+mod blockchain {
+    use super::*;
+
+    impl_test_v17__getblockchaininfo!();
+    impl_test_v17__getbestblockhash!();
+    impl_test_v17__getblock_verbosity_0!();
+    impl_test_v17__getblock_verbosity_1!();
+}
+
+// == Control ==
+mod control {
+    use super::*;
+
+    impl_test_v17__stop!();
+}
+
+// == Generating ==
+mod generating {
+    use super::*;
+
+    impl_test_v17__generatetoaddress!();
+}
+
+// == Network ==
+mod network {
+    use super::*;
+
+    impl_test_v17__getnetworkinfo!();
+}
+
+// == Rawtransactions ==
+mod raw_transactions {
+    use super::*;
+
+    impl_test_v17__sendrawtransaction!();
+}
+
+// == Wallet ==
+mod wallet {
+    use super::*;
+
+    impl_test_v17__createwallet!();
+    impl_test_v17__loadwallet!();
+
+    impl_test_v17__getnewaddress!();
+    impl_test_v17__getbalance!();
+    impl_test_v19__getbalances!();
+    impl_test_v17__sendtoaddress!();
+    impl_test_v17__gettransaction!();
+}
