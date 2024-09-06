279e0f922e37f740f06d76143255ec75e9e9e6da
Tobin C. Harding <me@tobin.cc>
2024-09-06 14:13:30 +1000

    v17: Implement JSONRPC method generate
    Implement the last JSONRPC  method from the `Generating` section of the
    docs.

--- integration_test/src/v17/generating.rs
+++ integration_test/src/v17/generating.rs
@@ -5,15 +5,32 @@
 //! Specifically this is methods found under the `== Generating ==` section of the
 //! API docs of `bitcoind v0.17.1`.
 
-/// Requires `Client` to be in scope and to implement `get_blockchain_info`.
+/// Requires `Client` to be in scope and to implement `generate_to_address`.
 #[macro_export]
 macro_rules! impl_test_v17__generatetoaddress {
     () => {
         #[test]
         fn generate_to_address() {
+            const NBLOCKS: usize = 1;
+
             let bitcoind = $crate::bitcoind_with_default_wallet();
             let address = bitcoind.client.new_address().expect("failed to get new address");
-            let json = bitcoind.client.generate_to_address(1, &address).expect("generatetoaddress");
+            let json = bitcoind.client.generate_to_address(NBLOCKS, &address).expect("generatetoaddress");
+            json.into_model().unwrap();
+        }
+    };
+}
+
+/// Requires `Client` to be in scope and to implement `generate`.
+#[macro_export]
+macro_rules! impl_test_v17__generate {
+    () => {
+        #[test]
+        fn generate() {
+            const NBLOCKS: usize = 100;
+
+            let bitcoind = $crate::bitcoind_with_default_wallet();
+            let json = bitcoind.client.generate(NBLOCKS).expect("generate");
             json.into_model().unwrap();
         }
     };
